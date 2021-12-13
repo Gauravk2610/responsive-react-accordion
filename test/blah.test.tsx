59d790927db29ddbd421825659f65299c633b1ed
Gaurav konde <63660013+Gauravk2610@users.noreply.github.com>
2021-12-14 01:38:11 +0530

    test file formatted

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -5,7 +5,14 @@ import { Accordion } from '../src';
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Accordion title='This is an accordion' content='This is hell awesome' spacing={10} />, div);
+    ReactDOM.render(
+      <Accordion
+        title="This is an accordion"
+        content="This is hell awesome"
+        spacing={10}
+      />,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
