19c4f2c001436ca933744cd2667d2b55f9322560
Gaurav konde <63660013+Gauravk2610@users.noreply.github.com>
2021-12-14 01:30:16 +0530

    css fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import './index.css'
 import { useState } from 'react';
+import styled from 'styled-components'
 
 interface accordionInterface {
   title: string,
@@ -27,20 +27,69 @@ export const Accordion = ({ title, content, openIcon, closeIcon, spacing }: acco
   const close_Icon = closeIcon ? closeIcon : minusIcon;
 
   return (
-    <div className="accordion-container" style={spacing ? { marginBottom: spacing} : {} }>
-      <div className='accordion'>
-        <div
+    <AccordionContainer className="accordion-container" style={spacing ? { marginBottom: spacing} : {} }>
+      <Accordionn className='accordion'>
+        <AccordionTitle
           onClick={() => setOpen(!open)} 
           className='accordion-title'
           >
             {title}
             {/* Title */} <div className='accordion-icon'>{ !open ? open_Icon : close_Icon }</div>
-          </div>
-        <div className={`accordion-collapsible ${open ? 'accordion-active' : 'accordion-inactive'}`}>
+          </AccordionTitle>
+        <AccordionCollapsible className={`accordion-collapsible ${open ? 'accordion-active' : 'accordion-inactive'}`}>
             {/* Content */}
             {content}
-        </div>
-      </div>
-    </div>
+        </AccordionCollapsible>
+      </Accordionn>
+    </AccordionContainer>
   );
 };
+
+
+const AccordionContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const Accordionn = styled.div`
+  max-width: 80vw;
+  width: 100%;
+  border: 2px solid black;
+  border-radius: 12px;
+
+  .accordion-active {
+    max-height: max-content;
+    padding-top: 16px;
+    padding-bottom: 16px;
+  }
+    
+  .accordion-inactive {
+    max-height: 0px;
+  }
+  
+`
+
+const AccordionTitle = styled.div`
+  cursor: pointer;
+  font-weight: 500;
+  display: flex;
+  justify-content: space-between;
+  padding: 16px 28px;
+  font-size: 20px;
+
+  @media(max-width: 768px) {
+    font-size: 28px;
+  }
+`
+
+const AccordionCollapsible = styled.div`
+    font-size: 20px;
+    padding: 0 28px;
+    overflow: hidden;
+    transition: all 300ms ease-in-out;
+    display: flex;
+
+  @media(max-width: 768px) {
+    font-size: 28px;
+  }
+`
